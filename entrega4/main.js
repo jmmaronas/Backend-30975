13f7646a0832d0ef2555cd21147bd14990643136
Juan Manuel Maroñas <jmmaronas@gmail.com>
2022-05-12 17:16:40 -0300

    corregido __dirname

--- entrega4/main.js
+++ entrega4/main.js
@@ -4,14 +4,16 @@ const app=express()
 
 const PORT=process.env.PORT || 8080
 
+console.log(__dirname + "/public")
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use("/",express.static(__dirname + "public"))
+app.use("/",express.static(__dirname + "/public"))
 
 app.use("/", router)
 
 app.get("/", (req, res)=>{
-    res.sendFile("./index.html")
+    res.sendFile(__dirname)
 })
 
 const server=app.listen(PORT, ()=>{
